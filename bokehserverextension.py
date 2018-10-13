d26a421215b0de7abe358847bcfc380b1c35c870
Henry S. Harrison <henry.schafer.harrison@gmail.com>
2018-10-13 15:51:55 -0400

    fix app paths

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -4,4 +4,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     apps = Path('apps').glob('*.py')
-    Popen(["bokeh", "serve", "--allow-websocket-origin=*", *(app.name for app in apps)])
+    Popen(["bokeh", "serve", "--allow-websocket-origin=*", *(str(app) for app in apps)])
