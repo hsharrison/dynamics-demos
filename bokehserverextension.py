e09e35e89d7910f7c35a5da9df7b6fda1ce805c9
Henry S. Harrison <henry.schafer.harrison@gmail.com>
2018-10-13 15:44:08 -0400

    serve all apps

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from subprocess import Popen
 
 
 def load_jupyter_server_extension(nbapp):
-    Popen(["bokeh", "serve", "hkb.py", "--allow-websocket-origin=*"])
+    apps = Path('apps').glob('*.py')
+    Popen(["bokeh", "serve", "--allow-websocket-origin=*", *(app.name for app in apps)])
