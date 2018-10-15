e727897ab22fe6fb927e99640b370bfc3bcc83e6
Henry S. Harrison <henry.schafer.harrison@gmail.com>
2018-10-15 17:24:53 -0400

    not every py file is an app

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from subprocess import Popen
 
+NOT_APPS = {'__init__.py', 'common.py'}
 
 def load_jupyter_server_extension(nbapp):
     apps = Path('apps').glob('*.py')
-    Popen(["bokeh", "serve", "--allow-websocket-origin=*", *(str(app) for app in apps)])
+    Popen(["bokeh", "serve", "--allow-websocket-origin=*", *(str(app) for app in apps if str(app) not in NOT_APPS)])
